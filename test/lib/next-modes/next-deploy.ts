5d7f0264ff44b0d0c2cf0ae66b440e8655595b30
pieh <misiek.piechowiak@gmail.com>
2026-07-18 19:58:01 +0200

    test: pass clear record of env vars for test instance that need to be configured for deployment

--- test/lib/next-modes/next-deploy.ts
+++ test/lib/next-modes/next-deploy.ts
@@ -73,6 +73,8 @@ export class NextDeployInstance extends NextInstance {
       NEXT_TEST_DIR: this.testDir,
       // Pass test-specific env vars
       ...this.env,
+      // Pass test-specific env vars in clear way to ensure they are set for deployment
+      NEXT_TEST_ENV: JSON.stringify({ ...this.env }),
     }
 
     const deployRes = await execa(deployScriptPath, [], {
